f8cf8f5f810b333e46e9372516e863642e8178c7
sidhhay <abrolsidhhay596@gmail.com>
2025-08-18 00:25:46 +0530

    Update ques4a.cpp

--- assignment2/assignment2/ques4a.cpp
+++ assignment2/assignment2/ques4a.cpp
@@ -2,8 +2,8 @@
 #include<string>
 using namespace std;
 int main(){
-    string s1="HIMANI";
-    string s2="NAYYAR";
+    string s1="SIDHHAY";
+    string s2="ABROL";
     string s3;
 int n=sizeof(s1)/sizeof(string);
 int m=sizeof(s2)/sizeof(string);
@@ -12,3 +12,4 @@ cout<<s3<<endl;
 
     return 0;
 }
+
