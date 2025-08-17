5a276be26a1121fc45f915143cc1593ce67852da
sidhhay <abrolsidhhay596@gmail.com>
2025-08-18 00:11:39 +0530

    Update ques4a.cpp

--- ques4a.cpp
+++ ques4a.cpp
@@ -2,8 +2,8 @@
 #include<string>
 using namespace std;
 int main(){
-    string s1="HIMANI";
-    string s2="NAYYAR";
+    string s1="SIDHHAY";
+    string s2="ABROL";
     string s3;
 int n=sizeof(s1)/sizeof(string);
 int m=sizeof(s2)/sizeof(string);
